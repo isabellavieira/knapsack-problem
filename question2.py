bef3dfb1e31268571cacb74b2ad840cf59f265e0
Rebecca Porphirio <rporphirio@gmail.com>
2016-11-23 15:03:18 -0200

    Correção final na questão 2

--- question2.py
+++ question2.py
@@ -12,14 +12,17 @@ def solve(instance_path):
         print('Solving', instance_name)
 
         start_time = timeit.default_timer()
-        items = knapsack(k, P, W)
+        items, dp_profit = knapsack(k, P, W)
         elapsed = timeit.default_timer() - start_time
 
         print_solution(P, W, items)
         print('Elapsed:', elapsed)
-        total = sum(W[item - 1] * fract for item, fract in items)
-        if total > k:
+        total_weight = sum(W[item - 1] * fract for item, fract in items)
+        if total_weight > k:
             raise Exception("Erro no preenchimento da mochila.")
+        total_profit = sum(P[item - 1] * fract for item, fract in items)
+        if total_profit != dp_profit:
+            raise Exception("Valor total nao alcancado.")
         print()
 
 def knapsack(k, P, W):
@@ -35,29 +38,27 @@ def knapsack(k, P, W):
 
     items = []
     i, j = n - 1, K - 1
-    while j > 0:
+    while j > 0 and m[i, j] > 0:
         p = m[i, j]
-        p_ = m[i - 1, j]
-        while p == p_ and m[i,j] != 0:
+        p_ = m[i-1, j]
+        while p == p_:
             i -= 1
-            p = m[i, j]
-            p_ = m[i - 1, j]
-        j -= W[i - 1]
+            p_ = m[i-1,j]
+        j -= W[i-1]
         if j >= 0:
             items.append((i, 1))
+            i -= 1
 
-    return items
-    # return m
+    return items, m[-1,-1]
 
 if __name__ == '__main__':
-    # from data import parse_instance
-    # k, P, W, C = parse_instance('data/Data-120-Q2.txt')
-    k = 5
-    P = [3,4,5,6]
-    W = [2,3,4,5]
+    from data import parse_instance
+    k, P, W, C = parse_instance('data/Data-120-Q2.txt')
+    # k = 7
+    # P = [1,4,5,7]
+    # W = [1,3,4,5]
     print(k)
-    items = knapsack(k, P, W)
+    items, dp_profit = knapsack(k, P, W)
+    print("Valor total da matriz: ",dp_profit)
     print(items)
     print_solution(P, W, items)
-    # m = knapsack(k, P, W)
-    # print(m)
--- question3.py
+++ question3.py
@@ -1,21 +1,28 @@
 from __future__ import print_function
 
 import timeit
+import numpy as np
 
 from data import instance_iterator, print_solution
 
 def solve(instance_path):
-    print('Question 3\n')
+    print('Question 2\n')
     for instance in instance_iterator(instance_path):
         instance_name, k, P, W, C = instance
         print('Solving', instance_name)
 
         start_time = timeit.default_timer()
-        items = knapsack(k, P, W, C)
+        items, dp_profit = knapsack(k, P, W)
         elapsed = timeit.default_timer() - start_time
 
         print_solution(P, W, items)
-        print('Elapsed: {:.6f}'.format(elapsed))
+        print('Elapsed:', elapsed)
+        total_weight = sum(W[item - 1] * fract for item, fract in items)
+        if total_weight > k:
+            raise Exception("Erro no preenchimento da mochila.")
+        total_profit = sum(P[item - 1] * fract for item, fract in items)
+        if total_profit != dp_profit:
+            raise Exception("Valor total nao alcancado.")
         print()
 
 def knapsack(k, P, W, C):
